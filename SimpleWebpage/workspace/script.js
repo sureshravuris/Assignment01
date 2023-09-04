532a35d673e344d77ea947ef726cb841a7799c87
sureshravuris <143477571+sureshravuris@users.noreply.github.com>
2023-09-04 00:34:35 -0700

    Add files via upload

---
+++ SimpleWebpage/workspace/script.js
@@ -0,0 +1,4 @@
+document.getElementById("subscribeButton").addEventListener("click", function() {
+    // Redirect to the subscription form or confirmation page
+    window.location.href = "subscription.html";
+});
